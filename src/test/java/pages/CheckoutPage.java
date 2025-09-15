b50d9c86d22897e8bc27478df9c822dc1d18aee5
Iqra <iqrahaider_khan@yahoo.com>
2025-09-14 23:40:57 -0400

    Add CheckoutPage object for handling user checkout flow

---
+++ src/test/java/pages/CheckoutPage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
+
+public class CheckoutPage {
+    private WebDriver driver;
+
+    private By firstName = By.id("first-name");
+    private By lastName = By.id("last-name");
+    private By postalCode = By.id("postal-code");
+    private By continueBtn = By.id("continue");
+    private By finishBtn = By.id("finish");
+
+    public CheckoutPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void fillCustomerInfo(String fName, String lName, String zip) {
+        driver.findElement(firstName).sendKeys(fName);
+        driver.findElement(lastName).sendKeys(lName);
+        driver.findElement(postalCode).sendKeys(zip);
+        driver.findElement(continueBtn).click();
+    }
+
+    public void finishCheckout() {
+        driver.findElement(finishBtn).click();
+    }
+}
+
+
